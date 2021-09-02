8ff7dd9ff26c1f652eba161b90da3c522d2c8092
SebastianZug <sebastian.zug@informatik.tu-freiberg.de>
2021-09-02 10:35:06 +0200

    feat: Data extractor output file
    Generates a csv file after finalizing aggregation process

--- src/data_extractor.py
+++ src/data_extractor.py
@@ -1,5 +1,7 @@
 from pathlib import Path
 import time
+import pandas as pd
+import numpy as np
 
 from github2pandas.issues import Issues
 from github2pandas.pull_requests import PullRequests
@@ -76,7 +78,16 @@ class Github_data_extractor():
     RAW_DATA_FOLDER = "raw_data"
 
     @staticmethod
-    def start(github_token, request_handler):
+    def start(github_token, request_handler, output_file_name = 'aggregation_history.csv'):
+
+        # Prepare data frame for providing aggregation history
+        status = pd.DataFrame()
+        repo_content = dict.fromkeys(request_handler.request.parameters.content, np.nan)
+        for index, repo in enumerate(request_handler.repository_list):
+            repo_content['repo_name'] = repo.full_name
+            status = status.append(repo_content, ignore_index=True)
+        status.set_index("repo_name", inplace=True)
+
         for content_element in request_handler.request.parameters.content:
             if content_element in Github_data_extractor.CLASSES:
                 number_of_repos = len(request_handler.repository_list)
@@ -94,7 +105,12 @@ class Github_data_extractor():
                     )
                     repo_base_folder.mkdir(parents=True, exist_ok=True)
                     Github_data_extractor.CLASSES[content_element](repo, repo_base_folder, github_token)
+                    status.loc[repo.full_name, content_element] = pd.Timestamp.now()
             else:
                 print(f"{content_element} not known in github2pandas toolchain!")
                 print("Please check spelling")
+                
+        output_path = Path(request_handler.request.parameters.project_folder, output_file_name)
+        file = open(output_path, 'w+', newline='')
+        status.to_csv(file)
         return True
